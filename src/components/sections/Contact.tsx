28e7d1d999e66902b5c39f6c470aecfdf49a90e3
Vincent <101572886+vinparwes@users.noreply.github.com>
2024-11-22 09:29:06 +0100

    Text revisions and some styling

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,9 +1,16 @@
-import { Box, Center, GridItem, Heading, ScaleFade, SimpleGrid, Stack } from "@chakra-ui/react"
-import LinkIconContainer from "../containers/LinkIconContainer"
-import { ReactComponent as GitHubIcon } from "../../assets/icons/GitHubIcon.svg"
-import { ReactComponent as LinkedInIcon } from "../../assets/icons/LinkedInIcon.svg"
-import { ReactComponent as MailIcon } from "../../assets/icons/MailIcon.svg"
-import MailIconContainer from "../containers/MailIconContainer"
+import {
+    Box,
+    Center,
+    Heading,
+    ScaleFade,
+    VStack,
+    Stack,
+} from "@chakra-ui/react";
+import LinkIconContainer from "../containers/LinkIconContainer";
+import { ReactComponent as GitHubIcon } from "../../assets/icons/GitHubIcon.svg";
+import { ReactComponent as LinkedInIcon } from "../../assets/icons/LinkedInIcon.svg";
+import { ReactComponent as MailIcon } from "../../assets/icons/MailIcon.svg";
+import MailIconContainer from "../containers/MailIconContainer";
 
 function ContactSection() {
     return (
@@ -11,54 +18,47 @@ function ContactSection() {
             <ScaleFade in={true} initialScale={0.9} delay={0.05}>
                 <Center>
                     <Box
-                        padding={'2vh'}
-                        mt={'2vh'}
-                        mr={'1vh'}
-                        borderRadius={'2vh'}
-                        position={'relative'}
-
+                        padding={"2vh"}
+                        mt={"2vh"}
+                        mr={"1vh"}
+                        borderRadius={"2vh"}
+                        position={"relative"}
                     >
                         <Stack>
                             <Center>
-                                <Heading mb={'2vh'}>Something cooler coming along shortly..</Heading>
+                                <Heading mb={"2vh"}>{"Get in touch! :)"}</Heading>
                             </Center>
                             <Center>
-                                <SimpleGrid columns={5} w={'50vh'}>
-                                    <GridItem w={'100%'}>
-                                        <ScaleFade in={true} initialScale={0.9} delay={0.3}>
-                                            <LinkIconContainer
-                                                aria="Github"
-                                                ContactIcon={GitHubIcon}
-                                                link="https://github.com/vinparwes"
-                                            />
-                                        </ScaleFade>
-                                    </GridItem>
-                                    <GridItem w={'100%'}>
-                                        <ScaleFade in={true} initialScale={0.9} delay={0.4}>
-                                            <LinkIconContainer
-                                                aria="LinkedIn"
-                                                ContactIcon={LinkedInIcon}
-                                                link="https://www.linkedin.com/in/vincent-parik-westlund-1815b1231"
-                                            />
-                                        </ScaleFade>
-                                    </GridItem>
-                                    <GridItem w={'100%'}>
-                                        <ScaleFade in={true} initialScale={0.9} delay={0.5}>
-                                            <MailIconContainer
-                                                aria="Mail"
-                                                ContactIcon={MailIcon}
-                                                link="vincentparikwestlund@gmail.com"
-                                            />
-                                        </ScaleFade>
-                                    </GridItem>
-                                </SimpleGrid>
+                                <VStack align="stretch">
+                                    <ScaleFade in={true} initialScale={0.9} delay={0.3}>
+                                        <LinkIconContainer
+                                            aria="Github"
+                                            ContactIcon={GitHubIcon}
+                                            link="https://github.com/vinparwes"
+                                        />
+                                    </ScaleFade>
+                                    <ScaleFade in={true} initialScale={0.9} delay={0.4}>
+                                        <LinkIconContainer
+                                            aria="LinkedIn"
+                                            ContactIcon={LinkedInIcon}
+                                            link="https://www.linkedin.com/in/vincent-parik-westlund-1815b1231"
+                                        />
+                                    </ScaleFade>
+                                    <ScaleFade in={true} initialScale={0.9} delay={0.5}>
+                                        <MailIconContainer
+                                            aria="Mail"
+                                            ContactIcon={MailIcon}
+                                            link="vincentparikwestlund@gmail.com"
+                                        />
+                                    </ScaleFade>
+                                </VStack>
                             </Center>
                         </Stack>
                     </Box>
                 </Center>
             </ScaleFade>
         </>
-    )
+    );
 }
 
-export default ContactSection
+export default ContactSection;
